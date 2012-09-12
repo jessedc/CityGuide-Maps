76ead63e93189043fd35aab460ba8a905c1f8205
Audrey Tam <amt@Audrey-Tams-MacBook-Air-3.local>
2012-09-12 10:41:51 +1000

    Initial Commit

---
+++ CityGuideWithCoreData/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  CityGuideWithCoreData
+//
+//  Created by Audrey Tam on 12/09/12.
+//  Copyright (c) 2012 Audrey Tam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
